f6771d4ad0f0fde9d042d16c23c8a93072366dca
ekarp <emilyrkarp@gmail.com>
2020-06-14 11:24:53 -0400

    webpack-dev-server

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
 
 function App() {
-    const URL = "http://127.0.0.1:8000/plants/plants/?format=json"
-    const [data, setData] = useState({ plants: [] });
+    const URL = "http://127.0.0.1:8000/plants/plants/?format=json";
+    const [data, setData] = useState([]);
 
     useEffect(() => {
         fetch(URL)
             .then((response) => response.json())
             .then((data) => {
+                console.log(data);
                 setData(data);
             });
     }, []);
 
     return (
-        <li>
-            {data.plants.map(plant => {
-                return <li key={plant.id}>{plant.name}: {plant.time_till_next_watering} days till water</li>
+        <ul>
+            {data.map((plant, index) => {
+                return <li key={index}>{plant.name}: {plant.time_till_next_watering} days till water</li>
             })}
-        </li>
+        </ul>
     )
 }
 
--- webpack.local.js
+++ webpack.local.js
@@ -1,50 +1,66 @@
 const path = require('path');
 const BundleTracker = require('webpack-bundle-tracker');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { HtmlWebpackPlugin } = require('html-webpack-plugin');
 
 module.exports = {
-  entry:  [path.join(__dirname, 'src/index'), path.join(__dirname, 'scss/index.scss')],
-  output: {
-    path: path.join(__dirname, './build'),
-    filename: 'index.js'
-  },
-  mode: 'development',
-  devtool: 'source-map',
-  plugins: [
-    new BundleTracker({
-      path: __dirname,
-      filename: 'webpack-stats.json'
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: [
-            {
-                loader: 'file-loader',
-                options: {
-                    name: './style.css',
-                }
-            },
-            { loader: 'extract-loader' },
-            { loader: 'css-loader?-url' },
-            { loader: 'postcss-loader' },
-            { loader: 'sass-loader' }
-        ]
-      }
+    entry:  [path.join(__dirname, 'src/index'), path.join(__dirname, 'scss/index.scss')],
+    output: {
+        path: path.join(__dirname, './build'),
+        filename: 'index.js',
+        crossOriginLoading: 'anonymous'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        // port: 3000,
+        host: 'localhost',
+        // historyApiFallback: true,
+        noInfo: false,
+        // stats: 'minimal',
+        // contentBase: '.',
+        publicPath: '/build',
+        // contentBase: path.join(__dirname, publicPath),
+        hot: true
+    },
+    plugins: [
+        // new BundleTracker({
+        //     path: __dirname,
+        //     filename: 'webpack-stats.json'
+        // }),
+        // new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+        // new HtmlWebpackPlugin()
     ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".es6"]
-  }
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.jsx?$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: './style.css'
+                        }
+                    },
+                    { loader: 'extract-loader' },
+                    { loader: 'css-loader?-url' },
+                    { loader: 'postcss-loader' },
+                    { loader: 'sass-loader' }
+                ]
+            }
+        ]
+    }
 };
